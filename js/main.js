743a187f1de145a008283498a86cd33d65d9b451
xkored <xkored@mail.ru>
2018-08-22 15:29:49 +0400

    added card page

--- js/main.js
+++ js/main.js
@@ -35,4 +35,55 @@ $(window).on('load', function (){
               scrollTop: 0
             }, "slow");
       });
+
+      ///// Карточка товара
+
+      //Левый блок навигации
+      $('.left__nav .sub').hide();
+      $('.left__nav span').click(function() {
+      $(this).toggleClass('active');
+      $(this).parent().find('.sub').toggleClass('active').slideToggle('slow');
+      return false;
+      });
+      //Tabs
+      $(document).ready(function () {
+      $(".tab-content__item").hide();
+      $(".tabs__container div.tab-content__item:first-child").show();
+          $('ul.tabs__list > li').click(function () {
+              if (!($(this).hasClass('active'))) {
+                  var thisLi = $(this);
+                  var numLi = thisLi.index();
+                  thisLi.addClass('active').siblings().removeClass('active');
+                  thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+              }
+          });
+      });
+      //Слайдер "Подобные товары компании"
+      $('.card__similar').owlCarousel({
+            nav: true,
+            items: 4,
+            loop: true,
+            dots: false,
+            margin:10,
+            navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
+      });
+      //Слайдер "Мы рекомендуем"
+      $('.card__recommended').owlCarousel({
+            nav: true,
+            items: 4,
+            loop: true,
+            dots: false,
+            margin:10,
+            navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
+      });
+      //Слайдер "Вы просматривали"
+      $('.card__seen').owlCarousel({
+            nav: true,
+            items: 4,
+            loop: true,
+            dots: false,
+            margin:10,
+            navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
+      });
+      
 });
