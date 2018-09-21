3a75c1b1f08af4447b15ef6d63731c449658e0e6
xkored <xkored@gmail.com>
2018-09-21 11:02:06 +0400

    updated files from original site

--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@ $(window).on('load', function (){
       $('.main-slider__inner').owlCarousel({
             nav: false,
             items: 1,
-            loop: false,
+            loop: true,
             dots: true,
             center: true,
             autoplay: true,
-            autoplayTimeout: 10000
+            autoplayTimeout: 10000,
+            smartSpeed: 1000
       });
       //Слайдер блока "Также покупают"
       $('.prod__inner').owlCarousel({
             nav: true,
             items: 4,
-            loop: true,
+            loop: false,
             dots: false,
             margin:10,
             navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
@@ -28,16 +29,17 @@ $(window).on('load', function (){
             navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
       });
       //Кнопка "Наверх"
-      
-      $('btn_to-top').click(function (e) {
+      $('.btn_to-top').click(function (e) {
       e.preventDefault();
       $('body, html').animate({
               scrollTop: 0
             }, "slow");
       });
-
-      ///// Карточка товара
-
+      //Триггер в Попап-Каталоге на кнопке "Закрыть"
+      $('.menu-close').click(function(e){
+          e.preventDefault();
+        $.fancybox.close();
+      });
       //Левый блок навигации
       $('.left__nav .sub').hide();
       $('.left__nav span').click(function() {
@@ -62,7 +64,7 @@ $(window).on('load', function (){
       $('.card__similar').owlCarousel({
             nav: true,
             items: 4,
-            loop: true,
+            loop: false,
             dots: false,
             margin:10,
             navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
@@ -71,7 +73,7 @@ $(window).on('load', function (){
       $('.card__recommended').owlCarousel({
             nav: true,
             items: 4,
-            loop: true,
+            loop: false,
             dots: false,
             margin:10,
             navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
@@ -85,5 +87,45 @@ $(window).on('load', function (){
             margin:10,
             navText: ["<img src='img/slider__arrow-prev.png'>", "<img src='img/slider__arrow-next.png'>"],
       });
-      
+      /*Запускаем submit на форме фильтра при изменении значения*/
+    $(function () {
+    
+      var selects = document.querySelectorAll('.sort-choice');
+      var filter = document.querySelector('#filter');
+      if (selects.length) {
+    
+        Array.prototype.forEach.call(selects, function (el) {
+          el.addEventListener('change', function () {
+            filter.submit();
+          })
+        })
+      }
+    
+    });
+    
+    //catalog list|tab view
+    // $('.catalog__view').click(function(){
+    //   $('.catalog-wrp').toggleClass('row');
+    // });
+    
+    //Госты и ТУ
+    $(function () {
+      $(".bold1").click(function () {
+        $(".gostPageContent").removeClass('sub_aktive');
+        $(this).parent().addClass('sub_aktive');
+        $(this).parent().next().slideUp();
+        if ($(this).parent().next().is(":visible")) {
+          $(this).parent().next().slideUp();
+          $(this).removeClass('active'); 
+        } else {   
+          $(this).parent().next().slideToggle();
+          $(this).addClass('active');    
+        }
+      });
+    });
+    
+    //Закрываем AjaxForm popup после успешной отправки
+    $(document).on('af_complete', function(event,res) {
+    	if(res.success) parent.$.fancybox.close();
+    });
 });
